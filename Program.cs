7d7365d585dcf806a4845fab42656377b18e4688
Majulia <majulialcarvalho@gmail.com>
2022-03-13 18:19:39 -0300

    creating a simple stopwatch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
